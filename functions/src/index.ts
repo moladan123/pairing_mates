37b9fdff7e27273d6f9fa06ae6a4cca487aa8ffd
Henry Dykhne <edykhne@uoguelph.ca>
2020-04-25 02:58:55 -0400

    first iteration of function to delete 30 minute old games

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,29 @@
 import * as functions from 'firebase-functions';
 
+// Import Admin SDK
+const admin = require("firebase-admin");
+admin.initializeApp();
+// Get a database reference to our posts
+const db = admin.database();
+
+exports.removeOldGames = functions.pubsub.schedule('every 30 minutes').onRun((context) => {
+    const date = new Date(Date.now() - 30 * 60 * 1000)  // 30 minutes ago
+    const oldGamesRef = db.collection('games').where('timeCreated', '<', date)
+    oldGamesRef.get().then((snapshot: any[]) => {
+        let removedGames:string = "";
+        snapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data());
+            doc.ref.delete();
+            removedGames += doc.id + "\n";
+        });
+        console.log("Removed Games: " + removedGames);
+    }).catch((err: any) => {
+        console.log('Error getting documents', err);
+    });
+    return null;
+});
+
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
